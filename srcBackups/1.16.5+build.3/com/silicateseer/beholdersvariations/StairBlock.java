e9847e442a8b4ffc04491505a94a25bc79a669ed
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2021-04-08 08:19:53 -0400

    Update Mappings

---
+++ srcBackups/1.16.5+build.3/com/silicateseer/beholdersvariations/StairBlock.java
@@ -0,0 +1,12 @@
+package com.silicateseer.beholdersvariations;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.StairsBlock;
+
+public class StairBlock extends StairsBlock{
+	
+	public StairBlock(Block base) {
+		super(base.getDefaultState(), FabricBlockSettings.copy(base));
+	}
+}
