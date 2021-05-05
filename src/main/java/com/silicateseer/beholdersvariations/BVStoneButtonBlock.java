4a1653271b12e6b4b81e3fee67c8c2632d185865
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2021-05-04 22:58:19 -0400

    New Classes

---
+++ src/main/java/com/silicateseer/beholdersvariations/BVStoneButtonBlock.java
@@ -0,0 +1,11 @@
+package com.silicateseer.beholdersvariations;
+
+import net.minecraft.block.StoneButtonBlock;
+
+public class BVStoneButtonBlock extends StoneButtonBlock{
+
+	protected BVStoneButtonBlock(Settings settings) {
+		super(settings);
+	}
+
+}
